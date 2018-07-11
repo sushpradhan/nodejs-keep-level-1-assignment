d16b17fcef009e05b0c94ff7f75ab9372b19e3e4
Dinesh Kumar <dinesh.1.kumar@stackroute.in>
2018-07-11 17:44:56 +0530

    modified problem.md

--- server/modules.js
+++ server/modules.js
@@ -1,6 +1,12 @@
 /* Replace undefined with Require of your Mongoose connection initialization method */
 const initializeMongooseConnection = undefined;
+/* Replace undefined with Require of your note entity*/
+const noteModel = undefined;
+/* Replace undefined with Require of your user entity*/
+const userModel = undefined;
 
 module.exports = {
-	initializeMongooseConnection
+	initializeMongooseConnection,
+	noteModel,
+	userModel
 }
