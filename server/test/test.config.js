64d08a36fad4db7309601eb17b06c26c7e6681b5
Amisha.Gupta <amisha.gupta@stackroute.in>
2019-07-12 17:36:34 +0530

    Update test.config.js

--- server/test/test.config.js
+++ server/test/test.config.js
@@ -1,2 +1,32 @@
 //  Test Configuration Object
+//  Test Configuration Object
+module.exports = {
+  note_user1: {
+    title: 'Angular needs to learn',
+    text: 'This is angular note',
+    status: 'not-started'
+  },
+  note_user2: {
+    title: 'React needs to learn',
+    text: 'This is react note',
+    status: 'not-started'
+  },
+  updateNote: {
+    title: 'Angular needs to learn',
+    text: 'This is angular 6 note',
+    status: 'not-started'
+  },
+  user: {
+    username: 'admin@gmail.com',
+    password: 'admin'
+  },
+  wrongPassword: {
+    username: 'admin@gmail.com',
+    password: 'admin1'
+  },
+  wrongUserName: {
+      username: 'admin1@gmail.com',
+      password: 'admin1'
+  }
+};
 
